2fc2778131d2ba9139005e0c122f399bd68a2f42
João Pedro <jpcm2@cin.ufpe.br>
2022-09-05 13:20:42 -0300

    onboarding

--- Vibefy/Controller/Onboarding/OnboardingPageFourViewController.swift
+++ Vibefy/Controller/Onboarding/OnboardingPageFourViewController.swift
@@ -26,8 +26,6 @@ class OnboardingPageFourViewController: UIViewController {
         button.addTarget(self, action: #selector(didTapStart), for: .touchUpInside)
         
         view.addSubview(button)
-        
-        // Do any additional setup after loading the view.
     }
     
     @objc private func didTapStart(){
@@ -38,15 +36,5 @@ class OnboardingPageFourViewController: UIViewController {
         
         present(nav, animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Vibefy/Controller/Onboarding/OnboardingPageThreeViewController.swift
+++ Vibefy/Controller/Onboarding/OnboardingPageThreeViewController.swift
@@ -13,18 +13,5 @@ class OnboardingPageThreeViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .green
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
