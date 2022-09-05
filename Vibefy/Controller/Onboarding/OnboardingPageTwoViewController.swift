656c03188709ac8eae0c87150f969447133a8101
João Pedro <jpcm2@cin.ufpe.br>
2022-09-05 15:00:07 -0300

    Tela 2 onboarding

--- Vibefy/Controller/Onboarding/OnboardingPageTwoViewController.swift
+++ Vibefy/Controller/Onboarding/OnboardingPageTwoViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class OnboardingPageTwoViewController: UIViewController {
 
+    private lazy var titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Você é o que você ouve. Sabemos disso."
+        label.font = UIFont(name: label.font.fontName, size: 36)
+        label.numberOfLines = 2
+        label.textColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var subTitleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "Por isso, consultamos o que você ouve no Apple Music."
+        label.font = UIFont(name: label.font.fontName, size: 24)
+        label.numberOfLines = 2
+        label.textColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private lazy var imageTwo: UIImageView = {
         let img = UIImage(named: "onBoarding2")
         let imgV = UIImageView(image: img)
@@ -28,7 +48,8 @@ class OnboardingPageTwoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupImages();
+        setupImages()
+        setupLabels()
     }
     
     func setupImages(){
@@ -41,5 +62,16 @@ class OnboardingPageTwoViewController: UIViewController {
         imageTwoBG.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
         imageTwoBG.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
         imageTwoBG.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
+    }
+    
+    func setupLabels(){
+        view.addSubview(subTitleLabel)
+        view.addSubview(titleLabel)
+        titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 130.0).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 25).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -25.0).isActive = true
+        subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8.0).isActive = true
+        subTitleLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 25).isActive = true
+        subTitleLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -25.0).isActive = true
     }
 }
