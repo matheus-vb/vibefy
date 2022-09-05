03ff83349f0d424a845e6607332ea2b3bea12b1d
Matheus Batista <matheus.vbatista04@gmail.com>
2022-09-05 13:25:38 -0300

    first onboarding page image fix

--- Vibefy/Controller/Home/HomeViewController.swift
+++ Vibefy/Controller/Home/HomeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let dragTable = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,16 +18,5 @@ class HomeViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Vibefy/Controller/Onboarding/OnboardingPageOneViewController.swift
+++ Vibefy/Controller/Onboarding/OnboardingPageOneViewController.swift
@@ -9,24 +9,10 @@ import UIKit
 
 class OnboardingPageOneViewController: UIViewController {
 
-    private lazy var titleLabel: UILabel = {
-        let view  = UILabel(frame: .zero)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 20
-        return view
-    }()
-    
-    private lazy var squareView: UIView = {
-        let view  = UIView(frame: .zero)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 20
-        return view
-    }()
-    
     private lazy var imageOne: UIImageView = {
         let img = UIImage(named: "onBoarding1")
         let imgV = UIImageView(image: img)
-        imgV.frame = CGRect(x: 0 , y: 0, width: 240, height: 300)
+        imgV.frame = CGRect(x: 0 , y: 0, width: 300, height: 300)
         return imgV
     }()
     
@@ -34,20 +20,13 @@ class OnboardingPageOneViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-        setupElements();
         setupPositions();
-    }
-    
-    
-    func setupElements(){
-        squareView.backgroundColor = .black
     }
     
     func setupPositions(){
         view.addSubview(imageOne)
         imageOne.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         imageOne.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        imageOne.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
-        imageOne.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
+        imageOne.translatesAutoresizingMaskIntoConstraints = false
     }
 }
